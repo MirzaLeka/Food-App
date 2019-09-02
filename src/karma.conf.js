177182019dd190cd9fcf23d78ad558721f86c416
Mirza Leka <mirza_376@hotmail.com>
2019-09-02 21:18:18 +0200

    Updating protractor and karma config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,8 +25,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    browsers: ['Chrome'],
+    customLaunchers: {
+    ChromeNoSandbox: {
+      base: 'Chrome',
+      flags: ['--no-sandbox']
+    }
+}
   });
 };
