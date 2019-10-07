c7931854361f17a963ad4da4676705c86af85fb7
Mirza Leka <mirza_376@hotmail.com>
2019-10-07 21:38:58 +0200

    Searching companies on input

--- src/app/components/sidebar-search-company/sidebar-search-company.component.ts
+++ src/app/components/sidebar-search-company/sidebar-search-company.component.ts
@@ -1,24 +1,52 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { OnInit,Component, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-sidebar-search-company',
   templateUrl: './sidebar-search-company.component.html',
   styleUrls: ['./sidebar-search-company.component.scss']
 })
-export class SidebarSearchCompanyComponent {
+export class SidebarSearchCompanyComponent implements OnInit {
 
-  @Output() companyNameEvent = new EventEmitter();
+  searchCompanyForm : FormGroup;
+  searchCompanyInput: FormControl;
+
+  @Output() companyNameEvent = new EventEmitter<string>();
 
   constructor() { }
 
-  handleKeyDown(keyCode: number, value: string) {
-    if ( keyCode === 13 ) {
-      this.searchCompany(value)
-    } 
+  createFormControls() { 
+    this.searchCompanyInput = new FormControl('');
   }
 
-  searchCompany(company: string) {
-    this.companyNameEvent.emit(company);
+  createForm() { 
+    this.searchCompanyForm = new FormGroup({
+      searchCompanyInput: this.searchCompanyInput
+    });
+  }
+
+  detectInputChanges() {
+    this.searchCompanyInput.valueChanges
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe(value => this.submitOnInput(value));
+  }
+
+  submitOnInput(text: string) {
+    this.companyNameEvent.emit(text);
+  }
+
+  handleSubmit() {
+    const { searchCompanyInput } = this.searchCompanyForm.value;
+    this.companyNameEvent.emit(searchCompanyInput);
+  }
+
+
+  ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+    this.detectInputChanges();
   }
 
 }
